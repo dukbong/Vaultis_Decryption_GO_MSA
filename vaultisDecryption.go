311da0b9b26df5e34641b2fc8920b2643f470f15
janghs <gorae930301@gmail.com>
2025-01-06 13:24:09 +0900

    랜덤 포트 할당

--- vaultisDecryption.go
+++ vaultisDecryption.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"log"
+	"net"
 	"vaultis-go-module/module/decryption"
 
 	"github.com/gin-gonic/gin"
@@ -8,10 +11,20 @@ import (
 )
 
 func main() {
+	// 사용 가능한 포트 동적 할당
+	listener, err := net.Listen("tcp", ":0") // 포트 0으로 바인딩하면 OS가 사용 가능한 포트를 자동으로 할당
+	if err != nil {
+		log.Fatalf("포트 바인딩 오류: %v", err)
+	}
+	port := listener.Addr().(*net.TCPAddr).Port
+
+	listener.Close()
+
+	// Eureka 클라이언트 설정
 	client := eureka.NewClient(&eureka.Config{
 		DefaultZone:           "http://127.0.0.1:8761/eureka/",
 		App:                   "decryption-go",
-		Port:                  8999,
+		Port:                  port, // 동적으로 할당된 포트 사용
 		RenewalIntervalInSecs: 10,
 		DurationInSecs:        30,
 		Metadata: map[string]interface{}{
@@ -29,5 +42,6 @@ func main() {
 	// Gin 라우터 설정
 	r := gin.Default()
 	r.POST("/decryption", decryption.Decrypt)
-	r.Run(":8999") // 포트 8999에서 HTTP 서버 실행
+	log.Printf("서버가 포트 %d에서 실행 중입니다.", port)
+	r.Run(fmt.Sprintf(":%d", port))
 }
